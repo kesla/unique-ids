93b89904bf65287348b6d4d42db33a14e4a28c7d
David Björklund <david.bjorklund@gmail.com>
2014-03-16 21:30:32 +0100

    use cornet instead of trumpet (it's about 10x faster)

--- unique-ids.js
+++ unique-ids.js
@@ -1,30 +1,52 @@
-var trumpet = require('trumpet')
+var Cornet = require('cornet')
+  , domutils = require('domutils')
+  , duplex = require('duplexer2')
+  , Parser = require('htmlparser2').WritableStream
+  , PassThrough = require('stream').PassThrough
 
   , uniqueIds = function () {
-      var tr = trumpet()
+      var cornet = Cornet()
         , idCounts = {}
+        , writable = new Parser(cornet)
+        , readable = new PassThrough()
+        , lastIndex = 0
 
-      tr.selectAll('*[id]', function (elm) {
-        elm.getAttribute('id', function (idString) {
-          idString = idString
-            .split(' ')
-            .map(function (id) {
-              if (idCounts[id] === undefined) {
-                idCounts[id] = 0
-              } else {
-                idCounts[id] = idCounts[id] + 1
-                id = id + '-' + idCounts[id]
-              }
-
-              return id
+      cornet.on('element', function (elm) {
+        // only care about the elm that have no parent - that are on the lowest
+        // level
+        if (!elm.parent)
+          process.nextTick(function () {
+            var index = cornet.dom.indexOf(elm) + 1
+            // slice it out of the dom to get all elements included
+            // (e.g. text-nodes etc)
+            cornet.dom.slice(lastIndex, index).forEach(function (e) {
+              readable.write(domutils.getOuterHTML(e))
             })
-            .join(' ')
-
-          elm.setAttribute('id', idString)
-        })
+            lastIndex = index
+          })
       })
 
-      return tr
+      cornet.on('dom', function () {
+        readable.end()
+      })
+
+      cornet.select('*[id]', function (elm) {
+        elm.attribs.id = elm.attribs.id
+          .split(' ')
+          .map(function (id) {
+            if (idCounts[id] === undefined) {
+              idCounts[id] = 0
+            } else {
+              idCounts[id] = idCounts[id] + 1
+              id = id + '-' + idCounts[id]
+            }
+
+            return id
+          })
+          .join(' ')
+      })
+
+      return duplex(writable, readable)
     }
 
 
