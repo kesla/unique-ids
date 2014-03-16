50785b39c9a30ee9d4e3c7edf3714b1571f9ef52
David Björklund <david.bjorklund@gmail.com>
2014-03-16 11:38:14 +0100

    rewrite and extend test

--- test.js
+++ test.js
@@ -1,21 +1,38 @@
-var assert = require('assert')
+var test = require('tape')
   , uniqueIds = require('./unique-ids')
 
-  , transformStream = uniqueIds()
-  , output = []
+test(function (t) {
+  var stream = uniqueIds()
+    , output = []
 
-transformStream.on('data', function (chunk) {
-  output.push(chunk)
+  stream.on('data', function (chunk) { output.push(chunk) })
+  stream.once('end', function () {
+    t.equal(output.join(''), '<h1 id="foo">Hello</h1>\n<p id="bar">Beep boop<span id="foo-1">woop woop!</span></p>\n<div id="foo-2">Some more foo</div>')
+    t.end()
+  })
+
+  stream.write('<h1 id="foo">Hello</h1>\n')
+  stream.write('<p id="bar">Beep boop')
+  stream.write('<span id="foo">woop woop!</span>')
+  stream.write('</p>\n')
+  stream.write('<div id="foo">Some more foo</div>')
+  stream.end()
 })
 
-transformStream.once('end', function () {
-  assert.equal(output.join(''), '<h1 id="foo">Hello</h1>\n<p id="bar">Beep boop<span id="foo-1">woop woop!</span></p>\n<p class="world">OMG!</p>\n<div id="foo-2">Some more foo</div>')
-})
+test(function (t) {
+  var stream = uniqueIds()
+    , output = []
 
-transformStream.write('<h1 id="foo">Hello</h1>\n')
-transformStream.write('<p id="bar">Beep boop')
-transformStream.write('<span id="foo">woop woop!</span>')
-transformStream.write('</p>\n')
-transformStream.write('<p class="world">OMG!</p>\n')
-transformStream.write('<div id="foo">Some more foo</div>')
-transformStream.end()
+  stream.on('data', function (chunk) {
+    output.push(chunk)
+  })
+
+  stream.once('end', function () {
+    t.equal(output.join(''), '<p class="hello">Hello!<em> world!</em></p>')
+    t.end()
+  })
+
+  stream.write('<p class="hello">Hello!<em> world!</em></p>')
+  stream.end()
+
+})
